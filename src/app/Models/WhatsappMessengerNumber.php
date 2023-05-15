9b2510bf55862bcabf1d052675fb936235ab6670
Erkan Özkök <erkanozkok@gmail.com>
2023-05-15 17:40:10 +0300

    Whatsmessenger api postman updated

--- src/app/Models/WhatsappMessengerNumber.php
+++ src/app/Models/WhatsappMessengerNumber.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\S3Trait;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -12,6 +13,7 @@ class WhatsappMessengerNumber extends BaseModel
 {
     use HasFactory;
     use SoftDeletes;
+    use S3Trait;
 
     protected $casts = [
         'is_active' => 'boolean',
@@ -35,10 +37,31 @@ class WhatsappMessengerNumber extends BaseModel
     }
 
     /**
+     * @return string
+     */
+    public function getQrcodeUrlAttribute(): string
+    {
+        return $this->generatePreSignedUrl($this->attributes['qrcode_url']);
+    }
+
+    /**
      * @return array
      */
     public function getScreenshotsAttribute(): array
     {
-        return Storage::allFiles('/whatsmessenger/screenshots/' . $this->instance_id);
+        $screenshots = Storage::allFiles('/whatsmessenger/screenshots/' . $this->instance_id);
+
+        if (empty($screenshots)) {
+            return [];
+        }
+
+        rsort($screenshots);
+        $screenshots = array_slice($screenshots, 0, 10);
+
+        foreach ($screenshots as $key => $screenshot) {
+            $screenshots[$key] = $this->generatePreSignedUrl($screenshot);
+        }
+
+        return $screenshots;
     }
 }
---
+++ src/app/Traits/S3Trait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Storage;
+
+trait S3Trait
+{
+    /**
+     * @param  string  $path
+     * @return string
+     */
+    private function generatePreSignedUrl(string $path): string
+    {
+        $client = Storage::disk('s3')->getClient();
+        $expire = "+60 minutes";
+        $command = $client->getCommand('GetObject', [
+            'Bucket' => config('filesystems.disks.s3.bucket'),
+            'Key' => $path,
+        ]);
+        $request = $client->createPresignedRequest($command, $expire);
+
+        return (string)$request->getUri();
+    }
+
+    /**
+     * @param  string  $path
+     * @return string
+     */
+    private function generateStorageUrl(string $path): string
+    {
+        return Storage::disk('s3')->url($path);
+    }
+}
